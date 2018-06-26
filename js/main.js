3ea89351268fd680bd7d4c343a763748a9b6c04e
Ajala Ayodotun <dotunajala@gmail.com>
2018-06-26 16:44:46 +0100

    The currency object is stored in indexDb for offline usage

--- js/main.js
+++ js/main.js
@@ -1,28 +1,66 @@
 const baseCurrency = document.querySelector('#base-currency');
 const toCurrency = document.querySelector('#to-currency');
+let showingCurrency = false;
 
+const dbPromise = idb.open('c-currency', 1, (upgradeDb) => {
+    keyVal = upgradeDb.createObjectStore('currencies', {keyPath: 'id'});
+})
 
-fetch("https://free.currencyconverterapi.com/api/v5/currencies")
+fetch("https://free.currencycoverterapi.com/api/v5/currencies")
     .then((res) => {
         return res.json();
     })
     .then((data) => {
-       const html = generateHtml(Object.values(data.results))
-       baseCurrency.innerHTML = html;
-       toCurrency.innerHTML = html;
-       baseCurrency.querySelector('[value="Nigerian Naira"]').selected = true;
-       toCurrency.querySelector('[value="United States Dollar"]').selected = true;
+        //generate and display html and
+       //populate the idb database with currency juice
+       const currencies = Object.values(data.results);
+       const html = generateHtml(currencies)
+       displayHtml(html);
+       //Flag to check is the currencies are being displayed to the user
+       //It will be useful for offline situations
+       showingCurrency = true;
+       dbPromise.then((db) =>{
+           if(!db) return;
+           const tx = db.transaction('currencies', 'readwrite');
+           const store = tx.objectStore('currencies')
+           currencies.forEach(currency => {
+               store.put(currency);
+           });
+       });
     })
 
 function generateHtml(stuffs) {
     return stuffs.map(stuff => {
         return `
-        <option value="${stuff.currencyName}">${stuff.id}</option>
+        <option value="${stuff.id}">${stuff.id}</option>
         `
-    }).join(' ');
+    }).sort().join(' ');
 }
 
+function displayHtml(html) {
+    baseCurrency.innerHTML = html;
+    toCurrency.innerHTML = html;
+    baseCurrency.querySelector('[value="NGN"]').selected = true;
+    toCurrency.querySelector('[value="USD"]').selected = true;
+}
+
+//This will get the currencies from the idb when app is offline
+if(!showingCurrency) {
+    dbPromise.then((db) => {
+        if(!db) return;
+        const tx = db.transaction('currencies');
+        const store = tx.objectStore('currencies');
+        store.getAll().then((currencies) => {
+            html = generateHtml(currencies);
+            displayHtml(html);
+            showingCurrency = true;
+        })
+    })
+}
+
+//TODO: Implement the service worker to cache static content
 //TODO: Implement the covertion itself
+
 
 
 
